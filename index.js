3212247c1e89bece342c7e331bd8977e87fe27b6
Amran-Lab <amran3210@gmail.com>
2021-04-30 11:38:40 +0100

    add functions to index

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello')
+function celsiusToFahrenheit(temp){
+    fahr = (temp*9/5) + 32
+    return fahr
+}
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+    module.exports = { celsiusToFahrenheit}
+    }
+    else{
+    window.celsiusToFahrenheit = celsiusToFahrenheit;
+    }
