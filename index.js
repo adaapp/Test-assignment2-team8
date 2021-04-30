e56b7778f6d03dc524c96117ebf6cdf2ee3a0a2c
Amran-Lab <amran3210@gmail.com>
2021-04-30 12:36:30 +0100

    adding fahrenheit to celsius function

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ function celsiusToFahrenheit(temp){
     fahr = (temp*9/5) + 32
     return fahr
 }
+function fahrenheitToCelsius(temp){
+    if (temp == undefined){
+        throw new Error("error empty value");
+    }
+    if (isNaN(temp)){
+        throw new Error("error no number inputted");
+    }  
+    celsius = (temp-32)*5/9
+    return celsius
+}
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
-    module.exports = { celsiusToFahrenheit}
+    module.exports = { celsiusToFahrenheit, fahrenheitToCelsius}
     }
     else{
     window.celsiusToFahrenheit = celsiusToFahrenheit;
+    window.fahrenheitToCelsius = fahrenheitToCelsius;
     }
